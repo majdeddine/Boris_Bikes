9fdd4da72bb3fce097a0ba9df604a6b05d703b95
Lewis <lewisyoul@gmail.com>
2017-10-25 12:32:33 +0100

    adds test to get bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
    it { is_expected.to respond_to(:release_bike) }
+   it 'gets a bike' do
+     station = DockingStation.new
+     expect(station.release_bike).to be_an_instance_of(Bike)
+   end
 end
-
-
-#describe 'Greeter' do
-#  it 'greets Rico' do
-#    expect(greet('Rico')).to eq 'Hello, Rico, how are you today?'
-#  end
-#end
