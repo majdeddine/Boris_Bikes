014ef6247cbe700ad9171ba43fc9751d63b772d6
majdeddine <majda-jambouri@hotmail.fr>
2017-10-25 15:57:02 +0100

    dock increments by 1 and intialize method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,16 @@
 class DockingStation
-
   attr_reader :docked_bikes
 
+  def initialize
+    @docked_bikes = 0
+  end
   def release_bike
     Bike.new
   end
 
   def dock
-    
+    @docked_bikes += 1
   end
+
 
 end
