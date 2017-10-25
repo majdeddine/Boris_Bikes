fa4714b8d339565995c8160c981601476db2dfbe
Lewis <lewisyoul@gmail.com>
2017-10-25 17:02:33 +0100

    passes dock argument test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,13 +4,14 @@ class DockingStation
   def initialize
     @docked_bikes = 0
   end
+
   def release_bike
     Bike.new
   end
 
-  def dock
-    @docked_bikes += 1
+  def dock(arg)
+    #@docked_bikes += 1
+    arg
   end
-
 
 end
