ef78eedc5cc27a2e5a3bc66cf2b47bc50c663ef6
Lewis <lewisyoul@gmail.com>
2017-10-25 14:54:13 +0100

    adds test to respond to dock

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
+
    it { is_expected.to respond_to(:release_bike) }
+
    it 'gets a bike' do
      station = DockingStation.new
      expect(station.release_bike).to be_an_instance_of(Bike)
+   end
+
+   it 'responds to dock' do
+     station = DockingStation.new
+     expect(station).to respond_to(:dock)
    end
 end
