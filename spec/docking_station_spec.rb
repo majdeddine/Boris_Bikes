3ff3171e7e0f9941e1cc8d38cb931a98d7166b88
Lewis <lewisyoul@gmail.com>
2017-10-25 18:13:37 +0100

    test to check if dock accepts instance of bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -19,18 +19,20 @@ describe DockingStation do
      expect(station).to respond_to(:docked_bikes)
    end
 
-   it 'checks if dock receives argument' do
+   it 'checks if dock receives argument and that argument is instance of Bike' do
      station = DockingStation.new
+     bike = Bike.new
      expect(station).to respond_to(:dock).with(1).argument
+     expect(station.dock(bike)).to be_an_instance_of(Bike)
    end
 
    xit 'docked_bikes returns one' do
      station = DockingStation.new
-     station.dock
-     expect(station.docked_bikes).to eq(1)
+     station.dock(0)
+     expect(station.docked_bikes).to eq(0)
    end
 
-   it 'returns the argument that passed to it' do
+   xit 'returns the argument that passed to it' do
      station = DockingStation.new
      bike = Bike.new
      expect(station.dock(bike)).to eq(bike)
