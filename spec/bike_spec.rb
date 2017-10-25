d9ee5454959b5a70e57bce3420a68972d2511251
majdeddine <majda-jambouri@hotmail.fr>
2017-10-25 12:35:54 +0100

    add working bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,9 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to(:working?) }
+
+  it "return a working bike" do
+    bike = Bike.new
+    expect(bike.working?).to eq(true)
+  end
 end
